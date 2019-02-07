237585b5dd5fed49f4b2465dd53683da8c994272
Andréas Livet <andreas.livet@gmail.com>
2019-02-07 08:54:30 +0100

    server : use ws instead of websocket

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const WebSocketServer = require('websocket').server;
+const WebSocket = require('ws');
 
 const SERVER_PORT = 3000;
 
@@ -8,8 +8,21 @@ const SERVER_PORT = 3000;
 app.use(express.static('public'));
 
 
-app.listen(SERVER_PORT, () => {
+const server = app.listen(SERVER_PORT, () => {
 	console.log(`Start server on port ${SERVER_PORT}`);
 });
 
-const wsServer = new WebSocketServer({httpServer: app});
+const wsServer = new WebSocket.Server({server, path: "/ws"});
+
+wsServer.on("connection", ws => {
+    console.log((new Date()) + ' Connection accepted.');
+
+    ws.on('message', (data) => {
+        // Broadcast to everyone else.
+        wsServer.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+});
