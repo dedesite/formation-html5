1444d6d1f55d2ce2c8f2a35ea386b4908306d01b
Andréas Livet <andreas.livet@gmail.com>
2019-02-08 11:31:30 +0100

    workflow : manage history and hash change

--- public/js/jquery.workflow.plugin.js
+++ public/js/jquery.workflow.plugin.js
@@ -1,13 +1,62 @@
 ($ => {
+    function manageHistory(el) {
+        const workflow = $(el).attr('data-workflow');
+        // Try to avoid having a first pushState but doesn't work with internal link
+        // if(window.history.state === null) {
+        //     console.log("replace", window.history);
+        //     window.history.replaceState({workflow}, workflow, `#${workflow}`);    
+        // } else {
+        //     window.history.pushState({workflow}, workflow, `#${workflow}`);  
+        // }
+        window.history.pushState({workflow}, workflow, `#${workflow}`);  
+    }
+
+    function activateElement(parent, el) {
+        parent.children("section.active").removeClass("active");
+        el.addClass("active");
+    }
+
+    function showActiveElement(parent, el) {
+        parent.children("section").not(".active").hide();
+        el.show();
+    }
+
+    function activateWorkflow(workflow) {
+        const toActivate = currentEl.children(`section[data-workflow='${workflow}']`);
+        activateElement(currentEl, toActivate);
+        showActiveElement(currentEl, toActivate);
+    }
+
+    let currentEl = null;
+    window.onpopstate = e => {
+        if(e.state !== null && e.state.workflow) {
+            activateWorkflow(e.state.workflow);
+        }
+    }
+
+    window.onhashchange = e => {
+        activateWorkflow(location.hash.substring(1));
+    }
+
     $.fn.workflow = function(action) {
+        currentEl = this;
         if (action === "next") {
             const nextActive = this.children("section.active").next();
-            this.children("section.active").removeClass("active");
-            nextActive.addClass("active");
+            if(nextActive.length === 0) {
+                return false;
+            }
+            activateElement(this, nextActive);
+        } else if(action === "prev") {
+            const prevActive = this.children("section.active").prev();
+            if(prevActive.length === 0) {
+                return false;
+            }
+            activateElement(this, prevActive);
         }
 
-        this.children("section").not(".active").hide();
-        this.children("section.active").first().show();
+        const activeSection = this.children("section.active").first();
+        showActiveElement(this, activeSection);
+        manageHistory(activeSection);
         return this;
     };
 })(jQuery);
